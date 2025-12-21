1595a03cdeae7201afe9d16d72e4e77bd776db1f
Zzcorp <49915184+Zzcorp@users.noreply.github.com>
2025-12-21 22:36:59 +0700

    Add files via upload

--- core/management/commands/import_csv_update.py
+++ core/management/commands/import_csv_update.py
@@ -171,7 +171,7 @@ class Command(BaseCommand):
                                 break
 
                         keywords = ''
-                        for field in ['keywords', 'mots_cles', 'mots_clés', 'tags', 'mot_cle', 'motcles', 'keyword']:
+                        for field in ['keywords', 'Mots clefs', 'mots clés', 'tags', 'mot_cle', 'motcles', 'keyword']:
                             if field in row and row[field]:
                                 keywords = str(row[field]).strip()
                                 break
